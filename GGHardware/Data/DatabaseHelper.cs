061caead2ddc2ed5af0fb6547dedd4fcf2b172e0
axelsilva17 <axelsilriq95@gmail.com>
2025-08-22 20:27:06 -0300

    Agregar archivos de proyecto.

---
+++ GGHardware/Data/DatabaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace GGHardware.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string connectionString;
+
+        public DatabaseHelper()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
